f8143e4a52bdace0aa89ff2b3f549d6a8a4a4e6d
Chaytan Arora <chaytanarora@gmail.com>
2023-06-07 22:20:45 +0530

    move meal monkey project to its separate repo

--- meal-monkey-project/src/utils/helper.js
+++
@@ -1,6 +0,0 @@
-export const filterData = (restaurants, searchText) => {
-	const data = restaurants.filter((restaurant) =>
-		restaurant?.data?.name?.toLowerCase()?.includes(searchText.toLowerCase())
-	);
-	return data;
-};
